f4ca91c30495ed89bd0e0b7d4cbd8fb5a9af0a6e
Andre Rother <andre@andrerother.de>
2021-12-17 23:04:43 +0100

    refactor(day14): Cleans up the code

--- app/src/main/java/de/andrerother/aoc/day14/ExtendedPolymerization.java
+++ app/src/main/java/de/andrerother/aoc/day14/ExtendedPolymerization.java
@@ -5,18 +5,33 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ExtendedPolymerization {
 
     public static void main(final String[] args) {
         ExtendedPolymerization instance = new ExtendedPolymerization();
-        Map<String, String> rules = instance.readRules();
-        String template = "OFSVVSFOCBNONHKFHNPK";
-        String polymer = instance.createPolymerFromTemplate(template, 10, rules);
+        List<String> rules = instance.readRules();
+        String polymerTemplate = instance.getTemplateFromRules(rules);
+        Map<String, String> ruleSet = instance.getRuleSet(rules);
+        String polymer = instance.createPolymerFromTemplate(polymerTemplate, 10, ruleSet);
         int difference = instance.calculateDifference(polymer);
         System.out.println("Part One Answer: " + difference);
+    }
+
+    private Map<String, String> getRuleSet(final List<String> content) {
+        Map<String, String> rules = new HashMap<>();
+        for (int i = 2; i < content.size(); i++) {
+            rules.put(content.get(i).substring(0, 2), content.get(i).substring(6));
+        }
+        return rules;
+    }
+
+    private String getTemplateFromRules(final List<String> content) {
+        return !content.isEmpty() ? content.get(0) : "";
     }
 
     public int calculateDifference(final String polymer) {
@@ -36,25 +51,24 @@ public class ExtendedPolymerization {
     }
 
     public String createPolymerFromTemplate(final String template, final int iterations, final Map<String, String> ruleSet) {
-        String tmpPolymer = template;
-        String tmpPolymer2 = "";
+        String currentTemplate = template;
+        String currentPolymer = "";
         String polymer = "";
-//        Map<String, String> ruleSet = ruleSet;
         System.out.println("Template: " + template);
         for(int i = 0; i < iterations; i++) {
-            for(int j = 0; j < tmpPolymer.length()-1; j++) {
-                String substring = tmpPolymer.substring(j,j+2);
+            for(int j = 0; j < currentTemplate.length()-1; j++) {
+                String substring = currentTemplate.substring(j,j+2);
                 for (Map.Entry<String, String> entry : ruleSet.entrySet()) {
                     if(entry.getKey().equalsIgnoreCase(substring)) {
-                        tmpPolymer2 += entry.getKey().charAt(0) + entry.getValue();
+                        currentPolymer += entry.getKey().charAt(0) + entry.getValue();
                     }
                 }
             }
-            tmpPolymer2 += tmpPolymer.charAt(tmpPolymer.length()-1);
-            tmpPolymer = tmpPolymer2;
-            System.out.println("After step " + (i+1) + ": " + tmpPolymer2);
-            polymer = tmpPolymer2;
-            tmpPolymer2 = "";
+            currentPolymer += currentTemplate.charAt(currentTemplate.length()-1);
+            currentTemplate = currentPolymer;
+            System.out.println("After step " + (i+1) + ": " + currentPolymer);
+            polymer = currentPolymer;
+            currentPolymer = "";
         }
         return polymer;
     }
@@ -69,31 +83,9 @@ public class ExtendedPolymerization {
         return calculations;
     }
 
-
-    private Map<String, String> createRuleSet() {
-        Map<String, String> demoSet = new HashMap<>();
-        demoSet.put("CH","B");
-        demoSet.put("HH","N");
-        demoSet.put("CB","H");
-        demoSet.put("NH","C");
-        demoSet.put("HB","C");
-        demoSet.put("HC","B");
-        demoSet.put("HN","C");
-        demoSet.put("NN","C");
-        demoSet.put("BH","H");
-        demoSet.put("NC","B");
-        demoSet.put("NB","B");
-        demoSet.put("BN","B");
-        demoSet.put("BB","N");
-        demoSet.put("BC","B");
-        demoSet.put("CC","N");
-        demoSet.put("CN","C");
-        return demoSet;
-    }
-
-    private Map<String, String> readRules() {
+    private List<String> readRules() {
         // Shamelessly taken from https://mkyong.com/java/java-read-a-file-from-resources-folder/
-        Map<String, String> rules = new HashMap<>();
+        List<String> rules = new ArrayList<>();
         String fileName = "rules.txt";
         ClassLoader classLoader = getClass().getClassLoader();
         InputStream inputStream = classLoader.getResourceAsStream(fileName);
@@ -105,8 +97,7 @@ public class ExtendedPolymerization {
                  BufferedReader reader = new BufferedReader(streamReader)) {
                 String line;
                 while ((line = reader.readLine()) != null) {
-                    //BN -> H
-                    rules.put(line.substring(0, 2), line.substring(6));
+                    rules.add(line);
                 }
             } catch (IOException e) {
                 e.printStackTrace();
